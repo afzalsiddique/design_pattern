7b971a431d808b7b8bb85884847215395110afd3
afzal <afzal.siddique99@gmail.com>
2021-07-17 03:13:03 +0600

    Decompressor

--- src/DecoratorPattern/Main.java
+++ src/DecoratorPattern/Main.java
@@ -2,11 +2,11 @@ package DecoratorPattern;
 
 public class Main {
     public static void main(String[] args) {
-        Text text = new Encrypt2(new Encrypt1(new PlainText("abcd")));
+        Text text = new Compressor(new Encrypt2(new Encrypt1(new PlainText("aaabbcc"))));
         String res= text.getText();
         System.out.println(res);
 
-        Text text2 = new Decrypt1(new Decrypt2(new PlainText("defg")));
+        Text text2 = new Decrypt1(new Decrypt2(new Decompressor(new PlainText("d3e2f2"))));
         String res2= text2.getText();
         System.out.println(res2);
     }
--- src/DecoratorPattern/Main3.java
+++ src/DecoratorPattern/Main3.java
@@ -2,10 +2,14 @@ package DecoratorPattern;
 
 public class Main3 {
     public static void main(String[] args) {
-        StringBuilder a = new StringBuilder();
-        a.append('a');
-        a.append(1);
-        String str = a.toString();
-        System.out.println(str);
+        String str = "d3e2f2";
+        char[] arr = str.toCharArray();
+        int n=arr.length;
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i+=2){
+            int cnt=arr[i+1]-48;
+            sb.append(String.valueOf(arr[i]).repeat(cnt));
+        }
+        System.out.println(sb.toString());
     }
 }
